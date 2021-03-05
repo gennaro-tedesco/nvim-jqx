b597dd0531a486f28437ce5e7dcb904b0fa8000e
Gennaro Tedesco <gennarotedesco@gmail.com>
2021-03-05 22:42:35 +0100

    improved look of floating window

--- lua/nvim-jqx/floating.lua
+++ lua/nvim-jqx/floating.lua
@@ -1,5 +1,10 @@
 local config = require("nvim-jqx.config")
 
+local function centre_string(s)
+   local shift = math.floor((vim.api.nvim_win_get_width(0) -#s) / 2)
+   return string.rep(' ', shift)..s
+end
+
 local function floating_window(geometry)
    -- create internal window
    local total_width = vim.api.nvim_get_option("columns")
@@ -27,17 +32,19 @@ local function floating_window(geometry)
 	  col = math.ceil(total_width - win_width)/2 - 1
    }
    local border_buf = vim.api.nvim_create_buf(false, true)
-   local border_lines = { '╔' .. string.rep('═', win_width) .. '╗' }
-   local middle_line = '║' .. string.rep(' ', win_width) .. '║'
+   local border_lines = { '╭' .. string.rep('─', win_width) .. '╮' }
+   local middle_line = '│' .. string.rep(' ', win_width) .. '│'
    for _=1, win_height do
 	  table.insert(border_lines, middle_line)
    end
-   table.insert(border_lines, '╚'..string.rep('═', win_width)..'╝')
+   table.insert(border_lines, '╰'..string.rep('─', win_width)..'╯')
    vim.api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
 
    vim.api.nvim_open_win(border_buf, true, border_opts)
+   vim.api.nvim_win_set_option(0, 'winhl', 'Normal:Normal')
    vim.api.nvim_open_win(buf, 1, win_opts)
    vim.api.nvim_win_set_option(0, 'wrap', config.geometry.wrap)
+   vim.api.nvim_win_set_option(0, 'winhl', 'Normal:Normal')
    vim.cmd('au BufWipeout <buffer> exe "silent bwipeout! "'..border_buf)
    return buf
 end
@@ -53,4 +60,5 @@ end
 return {
    floating_window = floating_window,
    set_json_opts = set_json_opts,
+   centre_string = centre_string,
 }
